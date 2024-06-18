4e6f24fa7edecca64f27d8149a3b24cc5a46669a
Rahul Kr Sharma <Rahusa960@gmail.com>
2024-06-18 12:28:51 +0530

    after adding e

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages, setup
+from typing import List
+
+
+def get_requirements() -> List[str]:
+    
+    requirements_list=List[str]=[]
+    
+    return requirements_list 
+
+
+
+
+setup(
+name="sensor",
+version='0.0.1',
+author='Raul Kura',
+author_email='rahulsa960@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements(),
+   
+)
